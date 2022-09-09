cfe7d3c7ea590bf8ccfe4380f5e0ed1c901b1024
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-09 04:19:16 -0700

    #include <stdlib.h> #include <stdio.h> /* more headers goes there */
    /**
     * main - Entry point
     *
     * printing the numbers as char separated by ,
     *
     * Return: Always 0 (Success)
     */
    int main(void)
    {
            int ascii = 48;
    
            int i, j, k, l;
    
            for (i = 0; i < 10 ; i++)
            {
                    for (j = 0 ; j < 10 ; j++)
                    {
                            for (k = i; k < 10 ; k++)
                            {
                                    for (l = j+1; l < 10 ; l ++)
                                    {
                                            putchar(ascii + i);
                                            putchar(ascii + j);
                                            putchar(' ');
                                            putchar(ascii + k);
                                            putchar(ascii + l);
                                            {
                                                    putchar(',');
                                                    putchar(' ');
                                            }
                                    }
                            }
                    }
            }
            putchar('\n');
    
            return (0);
    }
    
    '

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * printing the numbers as char separated by ,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ascii = 48;
+
+	int i, j, k, l;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			for (k = i; k < 10 ; k++)
+			{
+				for (l = j+1; l < 10 ; l ++)
+				{
+					putchar(ascii + i);
+					putchar(ascii + j);
+					putchar(' ');
+					putchar(ascii + k);
+					putchar(ascii + l);
+					if (i != 9 || j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
