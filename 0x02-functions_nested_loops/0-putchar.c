cc05393671f46da65fc07f9edadb6612710c1ff0
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-13 15:30:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	char[] str = "_putchar";
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
