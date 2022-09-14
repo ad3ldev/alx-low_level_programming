2b31b4625d127800f904f176043e7382081b47f3
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-14 05:56:41 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 fibonacci numbers
+ * Return: Always Success
+ */
+int main(void)
+{
+	unsigned long i, n1, n2, temp;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%uld, ", n1);
+	for (i = 0 ; i <= 97 ; i++)
+	{
+		printf("%uld", n2);
+		if (i != 47)
+			printf(", ");
+		temp = n2;
+		n2 = n1 + n2;
+		n1 = temp;
+	}
+	printf("\n");
+	return (0);
+}
+
