e99171940ea5697b16c869131d8f8e8b5d4c0a18
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-15 06:37:39 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz code
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int i, flag;
+
+	for (i = 1; i <= 100; i++)
+	{
+		flag = 1;
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			flag = 0;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+			flag = 0;
+		}
+		if (flag)
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+	}
+}
