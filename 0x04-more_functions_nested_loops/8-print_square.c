503b3f28e9e53c8116f0dbf371139d84c7cbf064
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-15 06:31:53 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,11 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 	_putchar('\n');
 	}
-	for (i = 0 ; i < size ; i++)
+	for (i = 0; i < size ; i++)
 	{
 		for (j = 0; j < size ; j++)
 		{
