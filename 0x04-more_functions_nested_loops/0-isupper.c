c7c55fb029a0df840de3c9dce3448064d6aa229f
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-15 03:39:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	int A;
+
+	A = 65;
+	if (c >= A && c < A+26)
+	{
+		return (1);
+	}
+	return (0);
+}
