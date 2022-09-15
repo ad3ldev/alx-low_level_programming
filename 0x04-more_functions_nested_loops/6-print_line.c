850e4a5c106efbc20e3955091c05cdb74480b1c8
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-15 06:12:39 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ for n times
+ * @n: number of times to print _
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
