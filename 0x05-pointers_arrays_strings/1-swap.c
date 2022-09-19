2f067de19546556fc9d76a1bb34a925832952dfd
Adel <a.adel.a.fattah@gmail.com>
2022-09-19 19:37:22 +0200

    void swap_int(int *a, int *b);

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values between 2 ints
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = &a;
+	*a = &b;
+	*b = temp;
+}
