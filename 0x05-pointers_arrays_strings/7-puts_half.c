2458a3336852000ecd976e0b51cf657f96f1b250
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-20 03:41:49 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half to the stdout
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int size, n, i;
+
+	size = 0;
+	while (str[size])
+	{
+		size++;
+	}
+	if (size % 2 != 0)
+		n = (size - 1) / 2;
+	else
+		n = size / 2;
+
+	for (i = n - 1; i < size; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
