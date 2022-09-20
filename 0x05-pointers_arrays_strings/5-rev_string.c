1d8817db93d5c21e8050e8ab765658b0dc9d30d4
Adel <a.adel.a.fattah@gmail.com>
2022-09-20 12:26:44 +0200

    void rev_string(char *s);

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - revesre a given string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int size, i;
+
+	char temp;
+
+	size = 0;
+	while (s[size])
+		size++;
+	for (i = 0; i <= size/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[size - i - 1];
+		s[size - i - 1] = temp;
+	}
+}
