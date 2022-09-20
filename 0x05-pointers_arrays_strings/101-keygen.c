1f0ec47956fe1f63ae1cd745dd9abbc124ff4ddb
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-20 06:09:34 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - generates random passwords for 101-crackme
+ * Return: zero
+ */
+int main(void)
+{
+	int sum;
+	char c;
+	putchar(2772 - 2464);
+	return (0);
+}
