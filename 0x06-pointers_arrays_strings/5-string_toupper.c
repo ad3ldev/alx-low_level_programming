63ad34e09ca670f20b486e0b13aeb61c033223f4
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-22 13:02:03 -0700

    char *string_toupper(char *)

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert lowercase to uppercase
+ * @str: string to be converted
+ * Return: char * of the string in upper case
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - ('a' - 'A');
+		i++;
+	}
+	return (str);
+}
