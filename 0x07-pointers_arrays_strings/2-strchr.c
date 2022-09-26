83cdefdf4b2591eb4e477026cb9c8403ddb44af6
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-26 12:59:02 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while (s[i])
+	for (i = 0; s[i] >= '\0' ; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
-		i++;
 	}
 	return ('\0');
 }
