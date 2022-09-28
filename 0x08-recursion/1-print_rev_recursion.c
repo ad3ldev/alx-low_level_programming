5d20a2648b7fcc71b34a280972c2180cf598135f
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-28 10:51:53 -0700

    void _print_rev_recursion(char *s)

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed. 
+ * Returns: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
