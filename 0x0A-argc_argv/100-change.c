5f297cd8d4a8c6ea7c889867d1c03de33d58e2c0
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-30 12:58:33 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,32 +24,17 @@ int main(int argc, char *argv[])
 	while (cents != 0)
 	{
 		if (cents >= 25)
-		{
 			cents -= 25;
-			coins++;
-		}
 		else if (cents >= 10)
-		{
 			cents -= 10;
-			coins ++;
-		}
 		else if (cents >= 5)
-		{
 			cents -= 5;
-			coins ++;
-		}
 		else if (cents >= 2)
-		{
 			cents -= 2;
-			coins ++;
-		}
 		else if (cents >= 1)
-		{
 			cents -= 1;
-			coins ++;
-		}	
+		coins++;
 	}
 	printf("%d\n", coins);
-
 	return (0);
 }
