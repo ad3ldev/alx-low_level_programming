5dd51753b7611e0c9d95481523bb75256d032fd8
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-30 12:12:35 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints multiply of 2 arguments
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	mult = atoi(argv[1]) * atoi(argv[2]);
+
+	return (0);
+}
