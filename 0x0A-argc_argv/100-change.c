bd9697afa3bef22438828f7fdd52d46c85644696
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-30 12:54:30 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints change in coins
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins, cents;
+
+	coints = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+	while (cents != 0)
+	{
+		if (cents >= 25)
+		{
+			cents -= 25;
+			coins++;
+		}
+		else if (cents >= 10)
+		{
+			cents -= 10;
+			coins ++;
+		}
+		else if (cents >= 5)
+		{
+			cents -= 5;
+			coins ++;
+		}
+		else if (cents >= 2)
+		{
+			cents -= 2;
+			coins ++;
+		}
+		else if (cents >= 1)
+		{
+			cents -= 1;
+			coins ++;
+		}	
+	}
+	printf("%d\n", coins);
+
+	return (0);
+}
