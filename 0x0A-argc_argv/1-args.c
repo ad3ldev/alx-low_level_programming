905160647d5f7391f39668a34b4d864537df6c1b
Adel ALX <a.adel.a.fattah@gmail.com>
2022-09-30 11:56:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments after name of program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)args;
+	printf("%d\n", argc-1);
+	return (0);
+}
