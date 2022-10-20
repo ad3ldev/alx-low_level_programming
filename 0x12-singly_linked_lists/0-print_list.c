6dd8a800fd8c31a078a19ebf464826269f228140
Adel ALX <a.adel.a.fattah@gmail.com>
2022-10-20 02:52:49 -0700

    include lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,11 @@
+#include "list.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -5,9 +13,9 @@ size_t print_list(const list_t *h)
 	while(h)
 	{
 		if (h->str == NULL)
-			_printf("[0] (nil\n)");
+			printf("[0] (nil\n)");
 		else
-			_pirntf("[%d] %s\n", h->len, h->str);
+			pirntf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
