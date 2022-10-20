081ff2858f5fc04b2b526baa45237bdc3a298c74
Adel ALX <a.adel.a.fattah@gmail.com>
2022-10-20 03:00:07 -0700

    nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,10 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil\n)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		count++;
