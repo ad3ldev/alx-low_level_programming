26bda964a03d0a69410e3e5ee73882c426b85845
Adel ALX <a.adel.a.fattah@gmail.com>
2022-10-20 03:21:23 -0700

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,8 +4,6 @@
  * list_len - Finds the number of elements in
  *            a linked list_t list.
  * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
- *
  * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
