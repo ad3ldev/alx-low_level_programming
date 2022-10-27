ab21cb7c9700097ab32e5bf18e7f3ab354d5fae8
Adel ALX <a.adel.a.fattah@gmail.com>
2022-10-27 13:08:30 -0700

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, len = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 	while (b[len])
 	{
 		if (b[len] != '0' && b[len] != '1')
