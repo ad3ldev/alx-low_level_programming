15e57c5bde0d07f6d5df8581940fe8230a21e68a
Adel ALX <a.adel.a.fattah@gmail.com>
2022-11-04 15:42:04 -0700

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
